7168ad89e5e95ecacc7ba09e8e9ea23377d68069
aayankhaan <aayankhan1243.lag@gmail.com>
2023-06-26 04:57:29 -0700

    Initial commit

---
+++ broken.py
@@ -0,0 +1,17 @@
+#Introduction
+print("Hi there!")
+print("I will reply yes or no to your question")
+
+#Input
+
+question = input("Ask me a question = ")
+
+#Picking Answer
+
+import random
+
+answer = "yes" , "no" , 
+
+#Output
+
+print(random.choice(answer))
